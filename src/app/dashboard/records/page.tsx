15c040ff433fb9198e610a79ca4b6c80bfc0dfb1
Bonekazz <hierrofernandes23@gmail.com>
2025-06-11 02:45:48 -0300

    feat secure records page

--- src/app/dashboard/records/page.tsx
+++ src/app/dashboard/records/page.tsx
@@ -1,10 +1,17 @@
 import { prisma } from "@/lib/prisma";
 import { RecordPage } from "./record-page";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
 export default async function Page() {
+  const { userId } = await auth();
+  if (!userId) return redirect("/sign-in");
+
   try {
     const records = await prisma.record.findMany({ 
-      where: {},
+      where: {
+        userId,
+      },
       orderBy: {date: "desc"},
       include: { categories: true }
     });
