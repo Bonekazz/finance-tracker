ceb9a2f3b1bc3295608bdcb07c1cf38d2c928135
Bonekazz <hierrofernandes23@gmail.com>
2025-06-11 02:51:27 -0300

    feat secure categories page

--- src/app/dashboard/categories/page.tsx
+++ src/app/dashboard/categories/page.tsx
@@ -1,10 +1,17 @@
 import { prisma } from "@/lib/prisma";
 import { CategoriesPage } from "./categories-page";
+import { redirect } from "next/navigation";
+import { auth } from "@clerk/nextjs/server";
 
 export default async function Page() {
+
+  const { userId } = await auth();
+  if (!userId) return redirect("/sign-in");
     
   try {
-    const categories = await prisma.category.findMany({});
+    const categories = await prisma.category.findMany({
+      where: { userId }
+    });
 
     return (
       <div className="w-full">
