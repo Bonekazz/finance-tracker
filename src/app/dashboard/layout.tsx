0e104a99a679d06e5be7373af5e71f651abb8933
Bonekazz <hierrofernandes23@gmail.com>
2025-07-04 19:15:46 -0300

    feat: mobile version layout

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -4,19 +4,23 @@ import { redirect } from "next/navigation";
 
 const nav = [
   {
-    title: "Inicio", href: "/home",
+    title: "Inicio", href: "/",
+    headerTitle: "Resumo Deste Mês",
     icon: House
   },
   {
     title: "Regist.", href: "/records",
+    headerTitle: "Registros",
     icon: CircleDollarSign 
   },
   {
     title: "Categ.", href: "/categories",
+    headerTitle: "Resumo Deste Mês",
     icon: LibraryBig
   },
   {
     title: "Análise", href: "/analytics",
+    headerTitle: "Resumo Deste Mês",
     icon: FileChartLine
   },
 ]
@@ -26,21 +30,23 @@ export default async function Layout({ children }: { children: React.ReactNode }
   const { userId } = await auth();
   if (!userId) return redirect("/sign-in");
 
-  // console.log("[ SERVER ] User id: ", userId);
-  
   return (
-    <div className="w-full h-[100vh] grid grid-rows-[1fr_10%]">
-      {children}           
-      <div className="w-full bg-slate-300 px-5">
-        <nav className="w-full h-full flex gap-3 items-center justify-between">
-        { nav.map((n, i) => (
-          <a key={i} href={n.href} className="flex flex-col items-center gap-2">
-            <n.icon />
-            <p>{n.title}</p>
-          </a>
-        ))}
+    <div className="w-full h-[100vh] bg-white">
+      <div className="w-full h-full px-[23px] pt-[41px] flex flex-col gap-[28px]">
+        {children}
+      </div>          
+
+      <div className="fixed bottom-0 left-0 right-0 z-50 w-full px-5 border-t border-[#CACACA] bg-white md:hidden">
+        <nav className="w-full h-full flex gap-3 items-center justify-between py-2">
+          {nav.map((n, i) => (
+            <a key={i} href={`/dashboard${n.href}`} className="flex flex-col items-center gap-1">
+              <n.icon />
+              <p className="text-xs">{n.title}</p>
+            </a>
+          ))}
         </nav>
       </div>
+
     </div>
   )
 }
