8ad8db767af230d8164ebfc7b7046335397e9ac0
Bonekazz <hierrofernandes23@gmail.com>
2025-07-03 23:16:00 -0300

    feat: update page with mobile design

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,9 +1,7 @@
-import DashboardPage2 from "./dashboard2";
-
 export default async function Page() {
   return (
     <div className="w-full">
-      <DashboardPage2/>
+      eae
     </div>
   )
 }
