7dc27de920d184b89825c4c4dac25e93ffb378fe
Bonekazz <hierrofernandes23@gmail.com>
2025-07-08 02:31:16 -0300

    wip(ui): dashboard page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,4 +1,12 @@
+"use client";
+
+import { Button } from "@/components/ui/button"
+import { Drawer, DrawerClose, DrawerContent, DrawerTrigger } from "@/components/ui/drawer"
+import IOSDrawer from "@/components/ui/ios-drawer"
+import { useState } from "react";
+
 export default function Page() {
+  const [ isOpen, setIsOpen ] = useState(false);
   return (
     <div className="w-full h-full flex flex-col gap-[28px]">
         
@@ -8,6 +16,10 @@ export default function Page() {
 
       { /** SALDO **/ }
       <div className="flex flex-col gap-[22px] px-[17px] py-[28px] rounded-2xl border border-[#CACACA]">
+        <Button onClick={() => setIsOpen(true)}>open</Button>
+        <IOSDrawer open={isOpen} onClose={() => setIsOpen(false)}>
+          <div>eeaee</div>
+        </IOSDrawer>
         <div className="flex flex-col">
           <p className="text-[20px] font-medium">Saldo</p>
           <p className="text-[36px] font-bold mt-[-10px]">R$ 2.380,00</p>
